dba833fa588b57dd509aef45ec5fb7d0ffe49471
Yongtao Li <lytsun@gmail.com>
2020-02-29 09:34:13 -0600

    solution for MP0

--- MP0.py
+++ MP0.py
@@ -2,6 +2,7 @@ import random
 import os
 import string
 import sys
+import re
 
 stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
             "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
@@ -29,7 +30,28 @@ def process(userID):
     indexes = getIndexes(userID)
     ret = []
     # TODO
-                    
+    #f = open("output.txt","w")
+    counts = dict()
+    lines = sys.stdin.readlines()     
+    dPattern = '|'.join(map(re.escape,delimiters))  
+    for i in range(len(indexes)):
+    # only process titles with certain indexes
+            # break into lines and remove '\n'
+            #f.write(str(indexes[i]))
+            #f.write("\n")
+            #f.write(lines[indexes[i]])
+            line = lines[indexes[i]].strip()
+            # break into words by delimiters
+            for word in re.split(dPattern, line):
+                # remove empty strings
+                if word != '':
+                    # covert to lowercase
+                    word = word.lower()
+                    if word not in stopWordsList:
+                        counts[word]=counts.get(word,0)+1   
+    #f.close
+    sortKeys = sorted(sorted(counts), key=counts.get, reverse=True)
+    ret = sortKeys[:20]
     for word in ret:
         print word
 
